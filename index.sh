54020d9f6f53884d4c128f199431008b6d5302a6
feizheng <1290657123@qq.com>
2016-09-24 01:32:48 +0800

    intial

---
+++ index.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+#brew install autojump
+[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
